87ecbdc52c0bbde768628a0b073831d4a2bb5fae
NandhipatiGanesh <162296386+ganeshadvait@users.noreply.github.com>
2025-03-14 17:15:52 +0530

    section removed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import HowItWorks from '../components/How/how';
 import HeroSection from '../components/hero/hero';
-import About from "@/components/about/about";
 
 export default function Home() {
   return (
     <>
     <HeroSection />
     <HowItWorks />
-    <About />
+   
     </>
   );
 }
