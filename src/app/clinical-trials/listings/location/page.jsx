5b036703a5086cfe3ec0e4a6a76810c409d6e2a5
ashokadvait <ashok@advaitlabs.com>
2025-03-21 14:12:35 +0530

    add more pages

--- src/app/clinical-trials/listings/location/page.jsx
+++ src/app/clinical-trials/listings/location/page.jsx
@@ -5,9 +5,13 @@ export default function LocationPage() {
   return (
     <div>
       {["Africa", "Asia", "Europe", "North America", "South America"].map(
-        (location) => (
-          <li>
-            <a href="/clinical-trials/listings/location/">{location}</a>
+        (continent) => (
+          <li key={continent}>
+            <a
+              href={`/clinical-trials/listings/location/continent/${continent}`}
+            >
+              {continent}
+            </a>
           </li>
         )
       )}
