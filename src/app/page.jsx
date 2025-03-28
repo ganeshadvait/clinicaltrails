2bb8e0fb273f2c673f97fbdbe42cd41f9429f572
NandhipatiGanesh <126049941+NandhipatiGanesh@users.noreply.github.com>
2025-03-28 16:02:29 +0530

    branding enhancements

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <>
     <HeroSection />
-    <GptChat />
+    
     <AboutinNumbers />
     <HowItWorks />
     <Featured />
@@ -25,6 +25,7 @@ export default function Home() {
     <Testimonials />
     <DCT />
     <Blogs />
+    <GptChat />
    <Global />
    <Faqs />
     </>
